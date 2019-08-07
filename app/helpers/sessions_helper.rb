6ca55fbc32deb52f6d43bb7429010db77b87a8cc
kei kokaji <acidjannedoor@yahoo.co.jp>
2019-08-07 08:33:03 +0000

    finish editing--users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -44,4 +44,19 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  # 記憶しているURL(またはデフォルトURL)にリダイレクトします。
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  # アクセスしようとしたURLを記憶します。
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
